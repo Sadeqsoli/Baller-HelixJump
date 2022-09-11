3094ebbe511d28502cb51b777adce639483d7a52
SadeQ Soli <solisadeq@gmail.com>
2022-09-11 15:59:28 +0430

    Adding controller's scripts.

---
+++ Baller/Assets/Scripts/Controllers/ShaftController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaftController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}//endclass
